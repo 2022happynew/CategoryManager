6b37c87b020a12f12503674f173972c804561f6e
zhushi <1404284467@qq.com>
2016-11-23 20:08:37 +0800

    first commit

---
+++ app/src/main/java/comx/example/zhushi/categorymanager/MainActivity.java
@@ -0,0 +1,13 @@
+package comx.example.zhushi.categorymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
